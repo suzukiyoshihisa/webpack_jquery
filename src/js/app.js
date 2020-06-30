888055d157e4cee9a69bc4317045d42f1f28f0fd
Yoshihisa Suzuki <mf37459@gmail.com>
2020-06-30 11:57:21 +0900

    画像のサイト

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,1 @@
-import log from "./modules/log";
 import "../scss/style.scss";
-
-const message = "hello world webpack";
-
-//consoleへの表示
-log("message");
-
-//jqueryによる値段の表示
-$(".title").text(message);
